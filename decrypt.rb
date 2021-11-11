f0a17b4ed493c5f8df8587e8c4a3790390bf450d
Joel Grant <joelmgrant@me.com>
2021-11-11 13:06:05 -0700

    Create decrypt.rb

--- decrypt.rb
+++ decrypt.rb
@@ -1,7 +1,15 @@
 require './lib/enigma'
-require './lib/encrypt'
+# require './lib/encrypt'
 
-file  = File.open('data/message.txt', "r")
+enigma = Enigma.new
 
-
-puts "TEST TEST #{ARGV}"
+# open_file = ARGV[0]
+# write_file = ARGV[1]
+# key = ARGV[2]
+# date = ARGV[3]
+#
+# file  = File.open(open_file, "r")
+# text = file.read
+# require 'pry'; binding.pry
+# enigma = Enigma.from_file(text, key, date)
+# require 'pry'; binding.pry
