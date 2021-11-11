0b00240fcdeaef57b35a5ce21cac42ff8f9209c1
Joel Grant <joelmgrant@me.com>
2021-11-10 21:05:44 -0700

    File Setup

---
+++ lib/enigma.rb
@@ -0,0 +1,12 @@
+class Enigma
+  def initialize
+
+  end
+
+  def encrypt(message, key, date)
+  end
+
+  def decrypt(message, key, date)
+  end
+
+end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,14 +1,24 @@
-require './enigma'
-require 'simplecov'
+require './lib/enigma'
 
 describe Enigma do
   before(:each) do
     @enigma = Enigma.new
+    require 'pry'; binding.pry
   end
 
   describe '#initialize' do
     it 'returns an instance of Enigma' do
+      require 'pry'; binding.pry
       expect(@enigma).to be_a Enigma
     end
   end
-end 
+
+  describe '#encrypt' do
+
+  end
+
+  describe '#decrypt' do
+
+  end
+
+end
