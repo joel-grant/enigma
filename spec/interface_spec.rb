3e2afe487dba1b180e28e3d4dbdffe83f742ddd0
Joel Grant <joelmgrant@me.com>
2021-11-13 14:22:51 -0700

    Setup Interface before/each

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -1,11 +1,17 @@
 require './lib/enigma'
-require './lib/interface'
+# require './lib/interface'
 
 describe Interface do
   before(:each) do
-    @file = File.open('message.txt')
+    @file = File.open('message.txt', 'r')
     @test = @file.read
     @args = ARGV
+    @interface = Interface.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
+  end
+  describe '' do
+    it 'contains and returns the file name to read' do
+
+    end
   end
 
 end
