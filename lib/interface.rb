932fc01e8bed66497da0e332e495d50a99e944ce
Joel Grant <joelmgrant@me.com>
2021-11-13 15:51:11 -0700

    Set up Interface class template

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,14 +1,21 @@
 class Interface
-  attr_reader :enigma, :in_file_name, :out_file_name, :key, :date, :message
+  attr_reader :enigma, :details, :in_file, :out_file, :message
 
   def initialize(message, details)
-    @enigma = Egnima.new
-    @in_file_name   = details[0]
-    @out_file_name  = details[1]
-    @key            = details[2]
-    @date           = details[3]
-    @message        = message
+    @enigma   = Egnima.new
+    @in_file  = details[0]
+    @out_file = details[1]
+    @message  = message
   end
 
-   
+  def start_enigma
+    encrypted_hash = @enigma.encrypt(@message)
+    display_output(encrypted_hash)
+  end
+
+  def display_output(result)
+    puts "Created #{@outfile} with key #{result[:key]} and date #{result[:date]}"
+  end
+
+
 end
