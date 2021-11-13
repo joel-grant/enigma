57933aa07e836322f3311d71cb301bdae3c9a0bf
Joel Grant <joelmgrant@me.com>
2021-11-13 16:54:21 -0700

    Create interface functionality

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -5,29 +5,30 @@ require './lib/interface'
 
 
 describe Interface do
-  let(:message) { "Mr. Rocky Mountain Wrinkleton" }
-  let(:args) { ARGV }
+  let(:message) { "hello world" }
+  let(:out_file) { "encrypted.txt" }
+  let(:in_file) { "outfile.txt" }
 
   before(:each) do
     @file = File.open('message.txt', 'r')
-    @test = @file.read
+    # @message = @file.read
     # @args = ARGV
-    @interface = Interface.new(ARGV[0], ARGV[1])
+    @interface = Interface.new(message, [in_file, out_file])
     @enigma = Enigma.new
-    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
   end
 
   describe '#initialize' do
-    it 'contains and returns the file name to read' do
+    it 'contains returns an instance of Interface' do
       expect(@interface).to be_a Interface
     end
 
-    it 'returns the file to read' do
-      expect(args[0]).to eq('message.txt')
-    end
+    # it 'returns the file to read' do
+    #   expect().to eq('message.txt')
+    # end
 
-    it 'returns the file to write to' do
-      expect(args[1]).to eq('encrypted.txt')
+    it 'returns the file to read ' do
+      expect(out_file).to eq('encrypted.txt')
     end
 
     it 'contains the text from the open file' do
@@ -35,11 +36,15 @@ describe Interface do
     end
   end
 
-  describe '#start_enigma' do #passes the needed information to enigma and
-    it 'returns the appropriate hash' do
-      # There will likely be an if/else statment in this method based on how
-      # many arguments come from ARGV
-      expect(@enigma.encrypt(args[0], args[1], args[2], args[3]))
+  # describe '#start_enigma' do
+  #   it 'returns the appropriate hash' do
+  #     expect(@interface.start_enigma(message, "02715", "040895")).to eq({ encryption: "keder ohulw", key: "02715", date: "040895" })
+  #   end
+  # end
+
+  describe '#display_output' do
+    it 'returns the hash that is used to display the output' do
+      expect(@interface.display_output({ encryption: "keder ohulw", key: "02715", date: "040895" })).to eq("Created 'encrypted.txt' with key 02715 and date 040895")
     end
   end
 
