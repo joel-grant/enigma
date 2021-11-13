eeb38a64c6f4d6737bb958e066975bf9b6251eb3
Joel Grant <joelmgrant@me.com>
2021-11-13 10:13:51 -0700

    Add first draft of encrypt/decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Enigma
-  attr_accessor :file, :text, :key, :date, :write
+  attr_accessor :file, :text, :key, :date, :write, :alphabet
 
   def initialize
     @file   = File.open('message.txt', 'r')
@@ -9,6 +9,7 @@ class Enigma
     @key    = ARGV[2]
     @date   = ARGV[3]
     @write  = ARGV[1]
+    @alphabet = ("a".."z").to_a << " "
   end
 
   # def self.from_file(text, key, date)
@@ -23,15 +24,91 @@ class Enigma
     today.strftime("%m%d%y")
   end
 
-  def encrypt(message, key, date)
+  # def key_shift(key)
+  #   a_key = key[0..1].to_i
+  #   b_key = key[1..2].to_i
+  #   c_key = key[2..3].to_i
+  #   d_key = key[3..4].to_i
+  #   [a_key, b_key, c_key, d_key]
+  # end
 
+  def offset_shift
 
-    # returns a hash of the encrypted text, the key and the date used.
   end
 
+  def shift
 
+  end
 
-  def decrypt(message, key, date)
+  def encrypt(message, key = new_key, date = current_date)
+    incoming_message = message.split("")
+    test_array = []
+    offset_shift = (date.to_i * date.to_i).to_s[-4..-1]
+    # key_shift(key)
+    a_key = key[0..1].to_i
+    b_key = key[1..2].to_i
+    c_key = key[2..3].to_i
+    d_key = key[3..4].to_i
+
+    a_offset = offset_shift[0].to_i
+    b_offset = offset_shift[1].to_i
+    c_offset = offset_shift[2].to_i
+    d_offset = offset_shift[3].to_i
+
+    a_shift = a_key + a_offset
+    b_shift = b_key + b_offset
+    c_shift = c_key + c_offset
+    d_shift = d_key + d_offset
+
+    shift_values = [a_shift, b_shift, c_shift, d_shift]
+    shifts = []
+    shift_values.each do |value|
+      shifts << value
+    end
+
+    encrypted_message = ""
+    incoming_message.each.with_index do |char|
+      index = @alphabet.index(char)
+      encrypted_message += @alphabet.rotate(shifts[0] + index)[0]
+      shifts.rotate!(1)
+    end
+    { :date => date, :encryption => encrypted_message, :key => key }
+  end
+
+  def decrypt(message, key, date = current_date)
+    incoming_message = message.split("")
+    test_array = []
+    offset_shift = (date.to_i * date.to_i).to_s[-4..-1]
+    # key_shift(key)
+    a_key = key[0..1].to_i
+    b_key = key[1..2].to_i
+    c_key = key[2..3].to_i
+    d_key = key[3..4].to_i
+
+    a_offset = offset_shift[0].to_i
+    b_offset = offset_shift[1].to_i
+    c_offset = offset_shift[2].to_i
+    d_offset = offset_shift[3].to_i
+
+    a_shift = a_key + a_offset
+    b_shift = b_key + b_offset
+    c_shift = c_key + c_offset
+    d_shift = d_key + d_offset
+    shift_values = [a_shift, b_shift, c_shift, d_shift]
+    shifts = []
+    shift_values.each do |value|
+      shifts << value
+    end
+
+    decrypted_message = ""
+    incoming_message.each.with_index do |char|
+      char_index = @alphabet.index(char)
+      decrypted_message += @alphabet.rotate(@alphabet.size - shifts[0] + char_index)[0]
+      shifts.rotate!(1)
+    end
+
+    { :date => date, :encryption => decrypted_message, :key => key }
+
   end
 
 end
