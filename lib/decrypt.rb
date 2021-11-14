35ec21fc19c9357fa8f88c41699400dd5ea6b5a6
Joel Grant <joelmgrant@me.com>
2021-11-13 22:15:55 -0700

    Relocate runner files

---
+++ lib/decrypt.rb
@@ -0,0 +1,8 @@
+require './lib/enigma'
+require './lib/interface'
+
+file = File.open(ARGV[0], 'r')
+message = file.read
+
+go_go_gadget_interface = Interface.new(message, ARGV)
+go_go_gadget_interface.start_enigma_decryption
