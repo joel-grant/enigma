573bb1daf336b858de8d259f785cf90e8b01b8a8
Joel Grant <joelmgrant@me.com>
2021-11-14 14:43:37 -0700

    Adjust runners for interface changes

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,5 +4,10 @@ require './lib/interface'
 file = File.open(ARGV[0], 'r')
 message = file.read
 
-go_go_gadget_interface = Interface.new(message, ARGV)
-go_go_gadget_interface.start_enigma_decryption
+interface = Interface.new(message, ARGV)
+decryption = interface.start_enigma_decryption
+interface.display_decryption(decryption)
+
+new_file = File.open(ARGV[1], 'w')
+new_file.write(decryption[:encryption])
+new_file.close
