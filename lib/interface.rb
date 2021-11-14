ac029d56cfc4cb95bc35c59822830d4903503865
Joel Grant <joelmgrant@me.com>
2021-11-13 22:15:00 -0700

    Feat: Add #start_enigma_decryption

--- lib/interface.rb
+++ lib/interface.rb
@@ -7,16 +7,36 @@ class Interface
     @enigma   = Enigma.new
     @in_file  = details[0]
     @out_file = details[1]
+    @key      = details[2]
+    @date     = details[3]
     @message  = message
   end
 
-  def start_enigma
+  def start_enigma_encryption
     encrypted_hash = @enigma.encrypt(@message)
-    display_count(encrypted_hash)
+    display_encryption(encrypted_hash)
+    write_to_file(encrypted_hash)
   end
 
-  def display_output(result)
-    display =  "Created \'#{@out_file}\' with key #{result[:key]} and date #{result[:date]}"
+  def start_enigma_decryption
+    decrypted_hash = @enigma.decrypt(@message, @key, @date)
+    display_decryption(decrypted_hash)
+    write_to_file(decrypted_hash)
+  end
+
+  def display_encryption(result)
+    display =  "Created \'#{@out_file}\' with the key #{result[:key]} and date #{result[:date]}"
     p display
+  end
+
+  def display_decryption(result)
+    display = "Created \'#{@out_file}\' with the key #{result[:key]} and date #{result[:date]}"
+    p display
+  end
+
+  def write_to_file(result)
+    new_file = File.open(@out_file, 'w')
+    new_file.write(result[:encryption])
+    new_file.close
   end
 end
