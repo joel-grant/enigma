f6912ede9595b9c94cf33dfafe45e98a8d1de6ff
Joel Grant <joelmgrant@me.com>
2021-11-15 09:16:49 -0700

    Split off tests for Shift class

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -19,43 +19,43 @@ describe Enigma do
     end
   end
 
-  describe '#create_shifts' do
-    it 'returns an array' do
-      key = "02715"
-      date = "040895"
-      expect(enigma.create_shifts(key, date)).to be_a Array
-    end
+  # describe '#create_shifts' do
+  #   it 'returns an array' do
+  #     key = "02715"
+  #     date = "040895"
+  #     expect(enigma.create_shifts(key, date)).to be_a Array
+  #   end
+  #
+  #   it 'must only return elements of type Integer' do
+  #     key = "02715"
+  #     date = "040895"
+  #     expect(enigma.create_shifts(key, date)[0]).to be_a Integer
+  #   end
+  #
+  #   it 'creates and returns an array of the final shifts for the encrypter' do
+  #     key = "02715"
+  #     date = "040895"
+  #     expect(enigma.create_shifts(key, date)).to eq([3, 27, 73, 20])
+  #   end
+  # end
 
-    it 'must only return elements of type Integer' do
-      key = "02715"
-      date = "040895"
-      expect(enigma.create_shifts(key, date)[0]).to be_a Integer
-    end
-
-    it 'creates and returns an array of the final shifts for the encrypter' do
-      key = "02715"
-      date = "040895"
-      expect(enigma.create_shifts(key, date)).to eq([3, 27, 73, 20])
-    end
-  end
-
-  describe '#create_offset' do
-    it 'creates an array of each offset digit' do
-      expect(enigma.create_offset(encrypt_return[:date])).to eq("1025")
-    end
-  end
-
-  describe '#key_shift' do
-    it 'returns an array of the 4 key shifts' do
-      expect(enigma.key_shift(encrypt_return[:key])).to eq(["02", "27", "71", "15"])
-    end
-  end
-
-  describe '#offset_shift' do
-    it 'returns an array of the 4 offset shifts' do
-      expect(enigma.offset_shift(encrypt_return[:date])).to eq(["1", "0", "2", "5"])
-    end
-  end
+  # describe '#create_offset' do
+  #   it 'creates an array of each offset digit' do
+  #     expect(enigma.create_offset(encrypt_return[:date])).to eq("1025")
+  #   end
+  # end
+  #
+  # describe '#key_shift' do
+  #   it 'returns an array of the 4 key shifts' do
+  #     expect(enigma.key_shift(encrypt_return[:key])).to eq(["02", "27", "71", "15"])
+  #   end
+  # end
+  #
+  # describe '#offset_shift' do
+  #   it 'returns an array of the 4 offset shifts' do
+  #     expect(enigma.offset_shift(encrypt_return[:date])).to eq(["1", "0", "2", "5"])
+  #   end
+  # end
 
   describe '#new_key' do
     it 'returns the number as a string' do
