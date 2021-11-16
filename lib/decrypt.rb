120802937b503dd24da71e630e553f60aca9a185
Joel Grant <joelmgrant@me.com>
2021-11-15 21:08:09 -0700

    Relocate CLI output to runner files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,8 +6,9 @@ message = file.read
 
 interface = Interface.new(message, ARGV)
 decryption = interface.start_enigma_decryption
-interface.display_decryption(decryption)
 
 new_file = File.open(ARGV[1], 'w')
 new_file.write(decryption[:encryption])
 new_file.close
+
+p "Created \'#{ARGV[1]}\' with the key #{decryption[:key]} and date #{decryption[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,8 +6,9 @@ message = file.read
 
 interface = Interface.new(message, ARGV)
 encryption = interface.start_enigma_encryption
-interface.display_encryption(encryption)
 
 new_file = File.open(ARGV[1], 'w')
 new_file.write(encryption[:encryption])
 new_file.close
+
+p "Created \'#{ARGV[1]}\' with the key #{encryption[:key]} and date #{encryption[:date]}"
